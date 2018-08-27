cb0d856a0fdf7271e6c1252b8eaee03f2fd0c830
David Evans <divsyd@users.noreply.github.com>
2018-08-27 20:24:46 +1000

    Add more api getters

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-var Room = require('../models/room');
 var Hotel = require('../models/hotel');
-
-
+var Room = require('../models/room');
+var RoomInstance = require('../models/roominstance');
 
 
 /* GET api listing. */
@@ -12,13 +11,39 @@ router.get('/', (req, res) => {
     res.send('api works');
 });
 
-// Get all posts
+// rooms
 router.get('/rooms', (req, res) => {
 
     // get all rooms
     Room.find({})
         .then(rooms => {
             res.status(200).json(rooms);
+        })
+        .catch(error => {
+            res.status(500).send(error)
+        });
+});
+
+// hotels
+router.get('/hotels', (req, res) => {
+
+    // get all hotels
+    Hotel.find({})
+        .then(hotels => {
+            res.status(200).json(hotels);
+        })
+        .catch(error => {
+            res.status(500).send(error)
+        });
+});
+
+// RoomInstance
+router.get('/roominstance', (req, res) => {
+
+    // get all rooms
+    RoomInstance.find({})
+        .then(roomInstance => {
+            res.status(200).json(roomInstance);
         })
         .catch(error => {
             res.status(500).send(error)
