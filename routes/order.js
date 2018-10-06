884b41c05f0581aeb736ffd2dfaf921f6bbaabf7
lingyingyang <41569566+lingyingyang@users.noreply.github.com>
2018-10-06 17:56:52 +1000

    Add Order CRUD operations

--- routes/order.js
+++ routes/order.js
@@ -15,6 +15,7 @@ router.get('/',(req, res) => {
       res.status(500).send(error);
     });
 });
+
 // GET request by user id
 router.get('/:userId', (req, res) => {
   Order.findById({ 'user': req.params.userId })
@@ -24,6 +25,50 @@ router.get('/:userId', (req, res) => {
     .catch(error => {
       res.status(500).send(error);
     });
+});
+
+// create order
+router.post('/',(req, res) => {
+  const order = new Order({
+    roomInstance: req.body.roomInstance,
+    user: req.body.user,
+    numNights: req.body.numNights
+  });
+  order.save()
+    .then(order => {
+      res.status(201).json(order);
+    })
+    .catch(error => {
+      res.status(400).send(error);
+    })
+});
+
+// update order
+router.put('/',(req, res) => {
+  const order = new Order({
+    _id: req.body.id,
+    roomInstance: req.body.roomInstance,
+    user: req.body.user,
+    numNights: req.body.numNights
+  });
+  order.update()
+    .then(order => {
+      res.status(200).json(order);
+    })
+    .catch(error => {
+      res.status(400).send(error);
+    })
+});
+
+// delete request by order id
+router.delete('/delete/:id', (req, res) => {
+  Order.deleteOne({'_id': req.params.id})
+    .then(order => {
+      res.status(204).json(order);
+    })
+    .catch(error => {
+      res.status(400).send(error);
+    })
 });
 
 module.exports = router;
