c9fe5bd276469afc6b9aaafa5be33fe05a17cef8
David Evans <divsyd@users.noreply.github.com>
2018-10-08 19:35:40 +1100

    fix api url

--- src/app/servers/account/account-service.service.ts
+++ src/app/servers/account/account-service.service.ts
@@ -2,10 +2,11 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { catchError } from 'rxjs/operators';
-import { Observable} from 'rxjs';
+import { Observable } from 'rxjs';
 import { User } from '../../../../models/user';
 import { Router } from '@angular/router';
-import { Subject} from 'rxjs';
+import { Subject } from 'rxjs';
+import { environment } from './../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -17,9 +18,9 @@ export class AccountServiceService {
   private userId: string;
   private isAuthenticated = false;
   private authStatusListener = new Subject<boolean>();
-  private BASE_URL: String = 'http://localhost:8000/api/users/';
+  private BASE_URL = environment.apiUrl + '/users';  // URL to web api
   constructor(private http: HttpClient,
-              private router: Router ) { }
+    private router: Router) { }
 
   getToken() {
     return this.token;
@@ -46,26 +47,27 @@ export class AccountServiceService {
     }
   }
   login(user: User) {
-    this.http.post<{token: string}>(this.BASE_URL + 'login', user)
+    this.http.post<{ token: string }>(this.BASE_URL + 'login', user)
       .subscribe(response => {
         this.duplicated(response);
       }, error => {
-        this.authStatusListener.next(false);});
+        this.authStatusListener.next(false);
+      });
   }
   // Signup function
   signup(user: User) {
-    this.http.post<{token: string}>(this.BASE_URL + 'signup', user)
+    this.http.post<{ token: string }>(this.BASE_URL + 'signup', user)
       .subscribe(response => {
         this.duplicated(response);
-        }, error => {
-          this.authStatusListener.next(false);
-        });
+      }, error => {
+        this.authStatusListener.next(false);
+      });
   }
 
   duplicated(res) {
     const token = res.token;
     this.token = token;
-    if (token ) {
+    if (token) {
       const expiresDuration = res.expiresIn;
       this.isAuthenticated = true;
       this.authStatusListener.next(true);
@@ -77,43 +79,43 @@ export class AccountServiceService {
       this.saveToken(token, expiration, this.userEmail, this.userId);
       this.router.navigate(['/user']);
     }
-    }
-
-    logout() {
-      this.token = null;
-      this.isAuthenticated = false ;
-      this.authStatusListener.next(false);
-      this.clearToken();
-      this.router.navigate(['']);
-    }
-
-    private saveToken(token: string,
-                      expirationDate: Date,
-                      userEmail: string,
-                      userId: string) {
-      localStorage.setItem('token', token);
-      localStorage.setItem('expiration', expirationDate.toISOString());
-      localStorage.setItem('userEmail', userEmail);
-      localStorage.setItem('userId', userId);
-    }
-
-    private clearToken() {
-      localStorage.removeItem('token');
-      localStorage.removeItem('expiration');
-      localStorage.removeItem('userEmail');
-      localStorage.removeItem('userId');
-    }
-
-    private getLoginData() {
-      const token = localStorage.getItem('token');
-      const expiration = localStorage.getItem('expiration');
-      if (!token || !expiration) {
-        return null;
-      }
-      return {
-        token: token,
-        expiration: new Date(expiration)
-      };
-    }
   }
+
+  logout() {
+    this.token = null;
+    this.isAuthenticated = false;
+    this.authStatusListener.next(false);
+    this.clearToken();
+    this.router.navigate(['']);
+  }
+
+  private saveToken(token: string,
+    expirationDate: Date,
+    userEmail: string,
+    userId: string) {
+    localStorage.setItem('token', token);
+    localStorage.setItem('expiration', expirationDate.toISOString());
+    localStorage.setItem('userEmail', userEmail);
+    localStorage.setItem('userId', userId);
+  }
+
+  private clearToken() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expiration');
+    localStorage.removeItem('userEmail');
+    localStorage.removeItem('userId');
+  }
+
+  private getLoginData() {
+    const token = localStorage.getItem('token');
+    const expiration = localStorage.getItem('expiration');
+    if (!token || !expiration) {
+      return null;
+    }
+    return {
+      token: token,
+      expiration: new Date(expiration)
+    };
+  }
+}
 
