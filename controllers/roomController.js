0fdf41eb8afb9b2e7990feb47e2ee4920b569f60
David Evans <divsyd@users.noreply.github.com>
2018-10-16 16:57:13 +1100

    Update room api with sort and filter options

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -1,4 +1,6 @@
 var Room = require('../models/room');
+var mongoose = require('mongoose');
+
 
 // room api
 
@@ -15,19 +17,60 @@ function getRoom(req, res) {
 
 // Get all rooms and sorts by price. Also accepts optonal parameter to get rooms for specific 
 // hotel e.g.http://localhost:8000/api/room?hotel=5bc2d402ae96b33360e0a8dc
+
+
 function getRooms(req, res) {
-    let query = {}
+    let query = [];
+
+    // Allows sorting results by hotel and then price
+    if (req.query.sort) {
+        query.push(
+            {
+                $sort: {
+                    hotel: 1, price: 1
+                }
+            }
+        )
+    }
+
+    // Allows limiting results to specific hotel
     if (req.query.hotel) {
-        query = { hotel: req.query.hotel }
+        query.push(
+            {
+                $match: {
+                    hotel: mongoose.Types.ObjectId(req.query.hotel)
+                }
+            }
+        )
+    }
+
+    // Allows limiting the number of results returned
+    if (req.query.limit) {
+        query.push(
+            { $limit: +req.query.limit }
+        )
+    }
+
+    // If no options set, run a normal find. Otherwise, we use the aggregate pipline to filter/sort
+    if (query.length === 0) {
+        Room.find({})
+            .then(rooms => {
+                res.status(200).json(rooms);
+            })
+            .catch(error => {
+                res.status(500).send(error)
+            });
+    } else {
+        Room.aggregate(query)
+            .then(rooms => {
+                res.status(200).json(rooms);
+            })
+            .catch(error => {
+                res.status(500).send(error)
+            });
+
     };
-    Room.find(query)
-        .sort({ price: 1 })
-        .then(rooms => {
-            res.status(200).json(rooms);
-        })
-        .catch(error => {
-            res.status(500).send(error)
-        });
+
 };
 
 // search rooms
